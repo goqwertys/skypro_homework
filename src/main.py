340cb37de034638188962117c9a17e9f7b8d0064
Georgii Ovechkin <goqwertys@gmail.com>
2024-05-07 05:45:14 +0400

    tests added

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from src.masks import mask_account, mask_card
+from src.widget import get_masked_card_from_str
 
 
 def main() -> None:
@@ -9,6 +10,19 @@ def main() -> None:
     # MASK ACCOUNT
     acc_mask = mask_account("73654108430135874305")
     print(acc_mask)
+
+    strings_to_match = [
+        "Maestro 1596837868705199",
+        "Счет 64686473678894779589",
+        "MasterCard 7158300734726758",
+        "Счет 35383033474447895560",
+        "Visa Classic 6831982476737658",
+        "Visa Platinum 8990922113665229",
+        "Visa Gold 5999414228426353",
+        "Счет 73654108430135874305",
+    ]
+    for line in strings_to_match:
+        print(get_masked_card_from_str(line))
 
 
 if __name__ == "__main__":
