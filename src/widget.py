ce4ebc9b59ba46dff8710fba91c8134e5966ccb0
Georgii Ovechkin <goqwertys@gmail.com>
2024-05-07 05:21:13 +0400

    add get_masked_card_from_str

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,16 @@
 """
 Defining functions for widgets
 """
+from masks import mask_card, mask_account
+
+
+def get_masked_card_from_str(string: str) -> str:
+    """Returns masked """
+    letters = [char for char in string if not char.isdigit()]
+    nums = [char for char in string if char.isalpha()]
+    if len(nums) == 16:
+        return f"{letters}{mask_card(str(nums))}"
+    elif len(nums) == 20:
+        return f"{letters}{mask_account(str(nums))}"
+    else:
+        return ""
