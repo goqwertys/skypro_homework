23644f81b5b7647279a081e4868e0032aa01f5c3
Georgii Ovechkin <goqwertys@gmail.com>
2024-05-08 05:42:29 +0400

    convert_iso_ddmmyyy() added to widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,30 @@
 """
 Defining functions for widgets
 """
+
 from masks import mask_card, mask_account
 
 
 def mask_card_or_acc_sring(input_string: str) -> str:
     """Returns masked acc or card in string"""
     # Check if string is correct
-    if not len(input_string.split()[-1]) in [16, 20] and input_string.split()[-1].isdigit():
+    if (
+        not len(input_string.split()[-1]) in [16, 20]
+        and input_string.split()[-1].isdigit()
+    ):
         raise ValueError("Incorrect card or acc number")
     else:
         if input_string.split()[0] in {"Счёт", "Счет"}:
             return f"{input_string.split()[0]} {mask_account(input_string.split()[-1])}"
         else:
             return f"{" ".join(input_string.split()[:-1])} {mask_card(input_string.split()[-1])}"
+
+
+def convert_iso_ddmmyyy(iso_input: str) -> str:
+    """Accepts a ISO8601 string and returns string as dd.mm.yyyy"""
+    # ISO 8601: 2018-07-11T02:26:18.671407
+    iso_list = iso_input.split(sep="T")[0].split(sep="-")
+    year, mouth, date = iso_list
+    if len(iso_list) != 3:
+        raise ValueError("Incorrect data format")
+    return f"{date}.{mouth}.{year}"
