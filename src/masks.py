8042e2e39970d3c8a1dd924f0bf08f863d7cb83d
Georgii Ovechkin <goqwertys@gmail.com>
2024-05-08 05:08:31 +0400

    raise error in masks added

--- src/masks.py
+++ src/masks.py
@@ -8,7 +8,7 @@ def mask_card(card_number: str) -> str:
     if card_number.isdigit() and len(card_number) == 16:
         return f"{card_number[:5]} {card_number[5:7]}{"*" * 2} {"*" * 4} {card_number[12:]}"
     else:
-        return ""
+        raise ValueError("Incorrect card number")
 
 
 def mask_account(acc_number: str) -> str:
@@ -16,4 +16,4 @@ def mask_account(acc_number: str) -> str:
     if acc_number.isdigit() and len(acc_number) == 20:
         return f"{'*' * 2}{acc_number[-4::]}"
     else:
-        return ""
+        raise ValueError("Incorrect card number")
