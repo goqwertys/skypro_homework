47b6328ba6d722ff78d7d5d1d590b13b37d68870
Georgii Ovechkin <goqwertys@gmail.com>
2024-05-12 12:20:44 +0400

    sort_by_date() added

--- src/processing.py
+++ src/processing.py
@@ -4,3 +4,7 @@ from typing import List
 def filter_by_state(input_list: List[dict], state: str = "EXECUTED") -> List[dict]:
     """Returns filtered list of dicts if 'state' item contains state"""
     return [item for item in input_list if state in item.get("state")]
+
+
+def sort_by_date(input_list: List[dict]) -> List[dict]:
+    return sorted(input_list, key=lambda x: x.get('data', 0), reverse=True)
