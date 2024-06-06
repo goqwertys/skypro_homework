61707a76fd01327c92f065672a980289172ffdfb
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-07 03:28:45 +0400

    in card_number_generator added exception handling

--- src/generators.py
+++ src/generators.py
@@ -13,9 +13,10 @@ def transaction_descriptions(input_list: List[dict]) -> Iterator:
 
 def card_number_generator(start: int, end: int) -> Generator:
     """Generates card numbers in the format XXXX XXXX XXXX XXXX"""
-    if 0 < start < int("9" * 16) and 0 < end < int("9" * 16) and end > start:
-        num = start
-        while num <= end:
-            result = f"{"0" * (16 - len(str(num)))}{num}"
-            yield " ".join([result[0:4], result[4:8], result[8:12], result[12:]])
-            num += 1
+    if not (0 <= start <= int("9" * 16) and 0 <= end <= int("9" * 16) and end >= start):
+        raise ValueError("Incorrect start/end values")
+    num = start
+    while num <= end:
+        result = f"{'0' * (16 - len(str(num)))}{num}"
+        yield " ".join([result[0:4], result[4:8], result[8:12], result[12:]])
+        num += 1
