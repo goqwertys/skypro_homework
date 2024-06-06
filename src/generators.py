97f8e60f5163ba5c0aab1532f96eed582c56ec06
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-07 01:25:39 +0400

    transaction_descriptions() added to src.generators.py

--- src/generators.py
+++ src/generators.py
@@ -4,3 +4,7 @@ from typing import List, Iterator
 def filter_by_currency(input_list: List[dict], currency: str) -> Iterator:
     """Returns an iterator that yields, one by one, the transactions that specify the given currency"""
     return (tr for tr in input_list if tr["operationAmount"]["currency"]["code"] == currency)
+
+
+def transaction_descriptions(input_list: List[dict]) -> Iterator:
+    pass
