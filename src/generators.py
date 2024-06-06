b6d6dfbcc679b50c59865645e9ff58dfeb29ea08
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-07 01:21:40 +0400

    filter_by_currency() docstring added

--- src/generators.py
+++ src/generators.py
@@ -2,4 +2,5 @@ from typing import List, Iterator
 
 
 def filter_by_currency(input_list: List[dict], currency: str) -> Iterator:
+    """Returns an iterator that yields, one by one, the transactions that specify the given currency"""
     return (tr for tr in input_list if tr["operationAmount"]["currency"]["code"] == currency)
