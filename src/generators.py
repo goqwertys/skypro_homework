85a209ec46d717954ba9eaaa043bc250b74210d6
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-07 02:00:33 +0400

    card_number_generator() developed

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from typing import List, Iterator
+from typing import List, Iterator, Generator
 
 
 def filter_by_currency(input_list: List[dict], currency: str) -> Iterator:
@@ -11,6 +11,11 @@ def transaction_descriptions(input_list: List[dict]) -> Iterator:
     return (tr["description"] for tr in input_list)
 
 
-def card_number_generator(start: int, end: int) -> str:
+def card_number_generator(start: int, end: int) -> Generator:
     """Generates card numbers in the format XXXX XXXX XXXX XXXX"""
-    pass
+    if 0 < start < int("9"*16) and 0 < end < int("9"*16) and end > start:
+        num = start
+        while num <= end:
+            result = f"{"0" * (16 - len(str(num)))}{num}"
+            yield " ".join([result[0:4], result[4:8], result[8:12], result[12:]])
+            num += 1
