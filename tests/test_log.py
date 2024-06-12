77b65eec4d1df3b5ea113a01ad5045798192d601
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-13 03:17:30 +0400

    log() test updated

--- tests/test_log.py
+++ tests/test_log.py
@@ -15,11 +15,50 @@ def test_log_success_console(capsys):
 
 def test_log_error_console(capsys):
     @log()
-    def foo(x):
+    def foo_test(x):
         return 1 / x
 
     with pytest.raises(ZeroDivisionError):
-        foo(0)
+        foo_test(0)
     captured = capsys.readouterr()
-    expected_output = "foo error: ZeroDivisionError. Inputs: (0,), {}\n"
+    expected_output = "foo_test error: ZeroDivisionError. Inputs: (0,), {}\n"
     assert captured.out.rstrip() == expected_output.rstrip()
+
+
+def test_log_success_file():
+    log_file = "testlog.txt"
+    file_path = os.path.join("docs", log_file)
+
+    @log(filename=file_path)
+    def foo_test(x, y):
+        return x + y
+
+    foo_test(3, 4)
+
+    with open(file_path, "r") as f:
+        logs = f.read()
+
+    expected_output = "foo_test ok\n"
+    assert logs == expected_output
+
+    os.remove(file_path)
+
+
+def test_log_error_file():
+    log_file = "testlog.txt"
+    file_path = os.path.join("docs", log_file)
+
+    @log(filename=file_path)
+    def foo_test(x, y):
+        return x / y
+
+    with pytest.raises(ZeroDivisionError):
+        foo_test(1, 0)
+
+    with open(file_path, 'r') as f:
+        logs = f.read()
+
+    # f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs} "
+    expected_output = "foo_test error: ZeroDivisionError. Inputs: (1, 0), {}"
+
+    os.remove(file_path)
