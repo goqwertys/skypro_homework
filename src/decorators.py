4e6174471d1fc15eeb24f113e2e3562ab219981f
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-13 03:18:01 +0400

    log() updated

--- src/decorators.py
+++ src/decorators.py
@@ -5,19 +5,13 @@ from typing import Callable
 
 def log(*, filename: str | None = None) -> Callable:
     def decorator(func):
-        if filename is not None:
-            current_path = os.getcwd()
-            project_root = os.path.abspath(os.path.join(current_path, '..'))
-            os.chdir(project_root)
-            file_path = os.path.join('docs', filename)
-
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
                 log_message = f"{func.__name__} ok"
                 if filename:
-                    with open(file_path, 'a') as f:
+                    with open(filename, 'a') as f:
                         f.write(log_message + '\n')
                 else:
                     print(log_message)
@@ -25,7 +19,7 @@ def log(*, filename: str | None = None) -> Callable:
             except Exception as e:
                 log_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs} "
                 if filename:
-                    with open(file_path, 'a') as f:
+                    with open(filename, 'a') as f:
                         f.write(log_message)
                 else:
                     print(log_message)
