3d246f88b65a970cfec8474b144f587341d74afe
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-19 03:27:24 +0400

    test json file renamed/ test invalid file added

--- tests/test_get_operations_info.py
+++ tests/test_get_operations_info.py
@@ -5,7 +5,7 @@ from src.utils import get_operations_info
 
 
 def test_get_operations_info_basic(operations_info):
-    filename = "operations.json"
+    filename = "test_operations.json"
     filepath = os.path.join('docs', filename)
 
     assert get_operations_info(filepath) == operations_info
