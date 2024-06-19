2e96077f72ffb15500dd2befc6aa6d161ecb8554
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-19 04:21:34 +0400

    fixtures that creates temp files added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import pytest
 
 
@@ -106,6 +109,87 @@ def order_info_result():
 
 
 @pytest.fixture
+def transactions():
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
+
+
+@pytest.fixture
 def operations_info():
     return [
         {
@@ -142,3 +226,39 @@ def operations_info():
             'to': 'Счет 35383033474447895560'
         }
     ]
+
+
+@pytest.fixture
+def create_test_file_success(operations_info):
+    filename = "test_operations.json"
+    filepath = os.path.join('docs', filename)
+
+    # Создаем тестовый файл с операциями
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, 'w', encoding='utf-8') as f:
+        json.dump(operations_info, f, ensure_ascii=False, indent=4)
+    return filepath
+
+
+@pytest.fixture
+def create_invalid_test_file():
+    filename = "invalid_test_operations.json"
+    filepath = os.path.join('docs', filename)
+
+    # Create invalid json file
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, 'w', encoding='utf-8') as f:
+        f.write("{invalid_json: True,}")  # invalidJSON
+
+    return filepath
+
+
+@pytest.fixture
+def create_empty_test_file():
+    filename = "empty_test_operations.json"
+    filepath = os.path.join('docs', filename)
+
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, 'w', encoding='utf-8') as f:
+        f.write("")
+    return filepath
