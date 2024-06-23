700519be2aca8d8a5820ab628d391cd785803f6c
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-23 07:17:42 +0400

    test_get_operation_amount_eur() rewritten with fixture

--- tests/test_get_operation_amount.py
+++ tests/test_get_operation_amount.py
@@ -10,22 +10,8 @@ def test_get_operation_amount_rub(operations_info):
 
 
 @patch('requests.get')
-def test_get_operation_amount_eur(mock_get, operations_info):
+def test_get_operation_amount_eur(mock_get, operations_info, transaction_request_data):
     operation = operations_info[1]
-    mock_get.return_value.json.return_value = {
-        "date": "2018-02-22",
-        "historical": "",
-        "info": {
-            "rate": 148.972231,
-            "timestamp": 1519328414
-        },
-        "query": {
-            "amount": 8221.37,
-            "from": "USD",
-            "to": "RUB"
-        },
-        "result": 724703.41,
-        "success": True
-    }
+    mock_get.return_value.json.return_value = transaction_request_data
     assert get_operation_amount(operation) == 724703.41
     mock_get.assert_called_once()
