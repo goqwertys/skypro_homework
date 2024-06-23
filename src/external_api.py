5dec6eb8d20499af5c0a2441bdae2b4fa3d4574b
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-23 06:42:54 +0400

    get_operation_amount() added

---
+++ src/external_api.py
@@ -0,0 +1,29 @@
+import os
+from typing import Dict, Any
+
+import requests
+from dotenv import load_dotenv
+
+
+def get_operation_amount(transaction: Dict[str, Any]) -> float | None:
+    """Returns the amount of given transaction in rubles. Converts it if currency is in other currency"""
+    amount = float(transaction['operationAmount']['amount'])
+    currency = transaction.get('operationAmount').get('currency').get('code')
+
+    if currency == "RUB":
+        return amount
+
+    load_dotenv()
+    api_key = os.getenv('EXCHANGE_RATES_API_KEY')
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+    headers = {
+        "apikey": api_key
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return data.get("result")
+    except requests.RequestException as e:
+        print(f"Error fetching exchange rate data: {e}")
+        return 0.0  # Return 0.0 in case of any error
