4d374a6f5a9cf703111b45d24f5b2b3558503fb9
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-23 07:24:31 +0400

    test_get_operation_amount_bad_response() added not passed

--- tests/test_get_operation_amount.py
+++ tests/test_get_operation_amount.py
@@ -1,4 +1,9 @@
+import os
+
 import pytest
+import requests
+from dotenv import load_dotenv
+
 from src.external_api import get_operation_amount
 from unittest.mock import Mock
 from unittest.mock import patch
@@ -15,3 +20,17 @@ def test_get_operation_amount_eur(mock_get, operations_info, transaction_request
     mock_get.return_value.json.return_value = transaction_request_data
     assert get_operation_amount(operation) == 724703.41
     mock_get.assert_called_once()
+
+
+def test_get_operation_amount_bad_response(transaction_request_data):
+    with patch('requests.get') as mock_get:
+        mock_get.side_effect = requests.RequestException("Test exception")
+
+        result = get_operation_amount(transaction_request_data)
+
+        assert result == 0.0
+        load_dotenv()
+        api_key = os.getenv('EXCHANGE_RATES_API_KEY')
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=USD&amount=8221.37"
+        headers = {"apikey": api_key}
+        mock_get.assert_called_once_with(url, headers)
