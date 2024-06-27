de4ac099d227116c29bbfe17e170cfa89ed6f617
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-27 05:13:16 +0400

    log reports added to funcs

--- src/masks.py
+++ src/masks.py
@@ -20,14 +20,20 @@ logger.addHandler(fh)
 def mask_card(card_number: str) -> str:
     """Returns masked card number as string"""
     if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[12:]}"
+        masked_card = f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[12:]}"
+        logger.info(f"Masked card number: {masked_card}")
+        return masked_card
     else:
+        logger.error(f"Incorrect card number {card_number}")
         raise ValueError("Incorrect card number")
 
 
 def mask_account(acc_number: str) -> str:
     """Returns masked account number as string"""
     if acc_number.isdigit() and len(acc_number) == 20:
-        return f"{'*' * 2}{acc_number[-4::]}"
+        masked_acc = f"{'*' * 2}{acc_number[-4::]}"
+        logger.info(f"Masked account number: {masked_acc}")
+        return masked_acc
     else:
+        logger.error(f"Incorrect account number: {acc_number}")
         raise ValueError("Incorrect account number")
