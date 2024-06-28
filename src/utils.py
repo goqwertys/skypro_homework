9971b9d79c493ec23d543a36032b5d96acd0000d
Georgii Ovechkin <goqwertys@gmail.com>
2024-06-28 23:26:26 +0400

    some fixes

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import os
-from typing import Any, Dict, List, Hashable, Union
+from typing import Any, Dict, List, Union
 
 import pandas as pd
 
@@ -18,9 +18,9 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 
-def get_operations_info(path: str) -> Union[List[Dict[Hashable, Any]], List[Any]]:
-    """Takes as input a path to a JSON, CSV, or XLSX file
-    and returns a list of dictionaries with data about financial transactions"""
+def get_operations_info(path: str) -> Union[List[Dict[str, Any]], List[Any]]:
+    """Takes as input a path to a JSON, CSV, or XLSX file and returns a list of dictionaries with data about
+    financial transactions."""
     extension = path.split('.')[-1].lower()
 
     try:
@@ -33,15 +33,15 @@ def get_operations_info(path: str) -> Union[List[Dict[Hashable, Any]], List[Any]
             logger.warning(f"The data in {path} is not in the expected format. Expected a list of dictionaries.")
             return []
         elif extension == 'csv':
-            data = pd.read_csv(path)
+            data = pd.read_csv(path, delimiter=';')
             result = data.to_dict(orient='records')
             logger.info(f"Operations have been successfully loaded from {path}")
-            return result
+            return [dict(item) for item in result]  # Ensure the result is a list of dictionaries
         elif extension == 'xlsx':
             data = pd.read_excel(path)
             result = data.to_dict(orient='records')
             logger.info(f"Operations have been successfully loaded from {path}")
-            return result
+            return [dict(item) for item in result]  # Ensure the result is a list of dictionaries
         else:
             logger.warning(f'{extension} file is not supported. An empty list will be returned.')
             return []
